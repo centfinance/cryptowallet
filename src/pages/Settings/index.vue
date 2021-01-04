2d36341533067938bbb62c0e25b5457864ee0446
HS <cimran@gmail.com>
2021-01-04 22:24:23 +0000

    v1.0.24 Android playstore fix

--- src/pages/Settings/index.vue
+++ src/pages/Settings/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
     <AccountSettings />
+    <div class="settings-row">
+      <div>
+        Version
+      </div>
+      <div>
+        {{ version }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import AccountSettings from '@/components/AccountSettings';
+import { version } from '../../../package.json';
 
 export default {
   name: 'Settings',
   components: {
     AccountSettings,
+  },
+  data() {
+    return { version };
   },
 };
 
